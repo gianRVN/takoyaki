2520976422906c216a96126d422d39faeac41648
gianarvin <gian.arvin@mekari.com>
2023-08-12 11:30:52 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "./build",
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: "./build",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve("./index.html"),
+    }),
+  ],
+};
